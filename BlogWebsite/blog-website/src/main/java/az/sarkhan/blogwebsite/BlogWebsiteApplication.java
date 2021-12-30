74c0089703e204973cf7bdaf24e9268953df2f78
Serxan-Babayev-7599 <64852621+Serxan-Babayev-7599@users.noreply.github.com>
2021-12-30 20:06:19 +0400

    Project Created

---
+++ BlogWebsite/blog-website/src/main/java/az/sarkhan/blogwebsite/BlogWebsiteApplication.java
@@ -0,0 +1,13 @@
+package az.sarkhan.blogwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogWebsiteApplication.class, args);
+	}
+
+}
---
+++ BlogWebsite/blog-website/src/test/java/az/sarkhan/blogwebsite/BlogWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package az.sarkhan.blogwebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
