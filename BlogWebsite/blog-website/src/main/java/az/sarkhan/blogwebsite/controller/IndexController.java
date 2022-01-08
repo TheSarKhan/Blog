2cd0bf64d4b024e5c4b8b80f274ad0b290309605
Serxan-Babayev-7599 <64852621+Serxan-Babayev-7599@users.noreply.github.com>
2022-01-08 19:08:20 +0400

    Navigation bar added

--- BlogWebsite/blog-website/src/main/java/az/sarkhan/blogwebsite/controller/IndexController.java
+++ BlogWebsite/blog-website/src/main/java/az/sarkhan/blogwebsite/controller/IndexController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 
-    @GetMapping(path =  "/")
+    @GetMapping(path = { "/index","/","/home"})
     public String GetIndex() {
         return "index";
     }
