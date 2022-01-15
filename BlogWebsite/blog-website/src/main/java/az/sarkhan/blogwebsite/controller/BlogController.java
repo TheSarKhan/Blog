06e3204b69a439348e91e6f011a899dd28231fc2
Serxan-Babayev-7599 <64852621+Serxan-Babayev-7599@users.noreply.github.com>
2022-01-16 03:12:10 +0400

    UI Optimization and Blog page created

--- BlogWebsite/blog-website/src/main/java/az/sarkhan/blogwebsite/controller/BlogController.java
+++ BlogWebsite/blog-website/src/main/java/az/sarkhan/blogwebsite/controller/BlogController.java
@@ -1,11 +1,22 @@
 package az.sarkhan.blogwebsite.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import az.sarkhan.blogwebsite.model.Content;
+import az.sarkhan.blogwebsite.repo.ContentRepo;
 @Controller
 public class BlogController {
+    @Autowired
+    public ContentRepo contentRepo;
     @GetMapping(path = "/blog")
-    public String GetBlogPage(){
+    public String GetBlogPage(Model model){
+       List<Content>contents=contentRepo.findAll();
+        model.addAttribute("contents", contents);
     return "blog";
     }
 }
