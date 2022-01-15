fc244788508c7b2450d5fe5cd68776db2ae87503
Serxan-Babayev-7599 <64852621+Serxan-Babayev-7599@users.noreply.github.com>
2022-01-16 02:20:14 +0400

    Model created

---
+++ BlogWebsite/blog-website/src/main/java/az/sarkhan/blogwebsite/model/Content.java
@@ -0,0 +1,18 @@
+package az.sarkhan.blogwebsite.model;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+ 
+import lombok.Data; 
+@Data
+@Entity
+public class Content {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String header;
+    private String text;
+}
