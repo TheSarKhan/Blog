b6dae2cbc577c6d7c32e0a7dd55d729f93cab977
SarKhann <serxan.babayev.06@gmail.com>
2024-08-31 19:12:46 +0400

    Fixed and completed

--- BlogWebsite/blog-website/src/main/java/az/sarkhan/blogwebsite/config/SecurityConfig.java
+++ BlogWebsite/blog-website/src/main/java/az/sarkhan/blogwebsite/config/SecurityConfig.java
@@ -1,23 +1,16 @@
 package az.sarkhan.blogwebsite.config;
-import javax.sql.DataSource;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter; 
 
 @Configuration
 @EnableWebSecurity
  
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
-	@Autowired
-	private DataSource dataSource;
-	
+	 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
     http.csrf().disable().authorizeRequests().antMatchers(HttpMethod.GET,"/").permitAll() 
@@ -37,8 +30,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
     .and().logout().permitAll();
 }
      
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-    	 auth.jdbcAuthentication().dataSource(dataSource);
-    }
+    
 }
